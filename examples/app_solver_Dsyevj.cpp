0656c2ad964513ce9bd6bff02c2e53ea20f8b3d5
Stephen Cole Kleene <kleenelan2049@hotmail.com>
2024-04-02 18:51:20 +0800

    build example with cuda env

--- examples/app_solver_Dsyevj.cpp
+++ examples/app_solver_Dsyevj.cpp
@@ -1,5 +1,5 @@
-#include <hip/hip_runtime.h>
-#include <hipsolver/hipsolver.h>
+#include <cuda_runtime.h>
+#include <cusolverDn.h>
 
 #include <iostream>
 #include <random>
@@ -72,48 +72,57 @@ inline std::string format_range(const BidirectionalIterator begin, const Bidirec
     return sstream.str();
 }
 
-inline const char* hipsolverStatusToString(hipsolverStatus_t status)
+
+inline const char* cusolverStatusToString(cusolverStatus_t status)
 {
     switch(status)
     {
-        case HIPSOLVER_STATUS_SUCCESS: return "HIPSOLVER_STATUS_SUCCESS";
-        case HIPSOLVER_STATUS_NOT_INITIALIZED: return "HIPSOLVER_STATUS_NOT_INITIALIZED";
-        case HIPSOLVER_STATUS_ALLOC_FAILED: return "HIPSOLVER_STATUS_ALLOC_FAILED";
-        case HIPSOLVER_STATUS_INVALID_VALUE: return "HIPSOLVER_STATUS_INVALID_VALUE";
-        case HIPSOLVER_STATUS_MAPPING_ERROR: return "HIPSOLVER_STATUS_MAPPING_ERROR";
-        case HIPSOLVER_STATUS_EXECUTION_FAILED: return "HIPSOLVER_STATUS_EXECUTION_FAILED";
-        case HIPSOLVER_STATUS_INTERNAL_ERROR: return "HIPSOLVER_STATUS_INTERNAL_ERROR";
-        case HIPSOLVER_STATUS_NOT_SUPPORTED: return "HIPSOLVER_STATUS_NOT_SUPPORTED";
-        case HIPSOLVER_STATUS_ARCH_MISMATCH: return "HIPSOLVER_STATUS_ARCH_MISMATCH";
-        case HIPSOLVER_STATUS_HANDLE_IS_NULLPTR: return "HIPSOLVER_STATUS_HANDLE_IS_NULLPTR";
-        case HIPSOLVER_STATUS_INVALID_ENUM: return "HIPSOLVER_STATUS_INVALID_ENUM";
-        case HIPSOLVER_STATUS_UNKNOWN: return "HIPSOLVER_STATUS_UNKNOWN";
-#if (hipsolverVersionMajor == 1 && hipsolverVersionMinor >= 8) || hipsolverVersionMajor >= 2
-        case HIPSOLVER_STATUS_ZERO_PIVOT: return "HIPSOLVER_STATUS_ZERO_PIVOT";
-#endif
+    case CUSOLVER_STATUS_SUCCESS:
+      return "CUSOLVER_STATUS_SUCCESS";
+    case CUSOLVER_STATUS_NOT_INITIALIZED:
+      return "CUSOLVER_STATUS_NOT_INITIALIZED";
+    case CUSOLVER_STATUS_ALLOC_FAILED:
+      return "CUSOLVER_STATUS_ALLOC_FAILED";
+    case CUSOLVER_STATUS_INVALID_VALUE:
+      return "CUSOLVER_STATUS_INVALID_VALUE";
+    case CUSOLVER_STATUS_ARCH_MISMATCH:
+      return "CUSOLVER_STATUS_ARCH_MISMATCH";
+    case CUSOLVER_STATUS_MAPPING_ERROR:
+      return "CUSOLVER_STATUS_MAPPING_ERROR";
+    case CUSOLVER_STATUS_EXECUTION_FAILED:
+      return "CUSOLVER_STATUS_EXECUTION_FAILED";
+    case CUSOLVER_STATUS_INTERNAL_ERROR:
+      return "CUSOLVER_STATUS_INTERNAL_ERROR";
+    case CUSOLVER_STATUS_MATRIX_TYPE_NOT_SUPPORTED:
+      return "CUSOLVER_STATUS_MATRIX_TYPE_NOT_SUPPORTED";
+    case CUSOLVER_STATUS_NOT_SUPPORTED:
+      return "CUSOLVER_STATUS_NOT_SUPPORTED ";
+    case CUSOLVER_STATUS_ZERO_PIVOT:
+      return "CUSOLVER_STATUS_ZERO_PIVOT";
+    case CUSOLVER_STATUS_INVALID_LICENSE:
+      return "CUSOLVER_STATUS_INVALID_LICENSE";
     }
-    // We don't use default so that the compiler warns if any valid enums are missing from the
-    // switch. If the value is not a valid hipsolverStatus_t, we return the following.
-    return "<undefined hipsolverStatus_t value>";
+
+    return "<unknown>";
 }
 
-#define HIP_CHECK(condition)                                                                \
+#define CUDA_CHECK(condition)                                                                \
     {                                                                                       \
-        const hipError_t error = condition;                                                 \
-        if(error != hipSuccess)                                                             \
+        const cudaError_t error = condition;                                                 \
+        if(error != cudaSuccess)                                                             \
         {                                                                                   \
-            std::cerr << "An error encountered: \"" << hipGetErrorString(error) << "\" at " \
+            std::cerr << "An error encountered: \"" << cudaGetErrorString(error) << "\" at " \
                       << __FILE__ << ':' << __LINE__ << std::endl;                          \
             std::exit(error_exit_code);                                                     \
         }                                                                                   \
     }
 
-#define HIPSOLVER_CHECK(condition)                                                            \
+#define CUSOLVER_CHECK(condition)                                                            \
     {                                                                                         \
-        const hipsolverStatus_t status = condition;                                           \
-        if(status != HIPSOLVER_STATUS_SUCCESS)                                                \
+        const cusolverStatus_t status = condition;                                           \
+        if(status != CUSOLVER_STATUS_SUCCESS)                                                \
         {                                                                                     \
-            std::cerr << "hipSOLVER error encountered: \"" << hipsolverStatusToString(status) \
+            std::cerr << "cuSOLVER error encountered: \"" << cusolverStatusToString(status) \
                       << "\" at " << __FILE__ << ':' << __LINE__ << std::endl;                \
             std::exit(error_exit_code);                                                       \
         }                                                                                     \
@@ -154,34 +163,34 @@ int main(const int argc, char* argv[])
     init_matrix(A, n, lda);
 
     // 4. Set hipsolver parameters
-    const hipsolverEigMode_t  jobz = HIPSOLVER_EIG_MODE_VECTOR;
-    const hipsolverFillMode_t uplo = HIPSOLVER_FILL_MODE_LOWER;
+    const cusolverEigMode_t  jobz = CUSOLVER_EIG_MODE_VECTOR;
+    const cublasFillMode_t uplo = CUBLAS_FILL_MODE_LOWER;
 
-    hipsolverSyevjInfo_t params;
-    HIPSOLVER_CHECK(hipsolverCreateSyevjInfo(&params));
-    HIPSOLVER_CHECK(hipsolverXsyevjSetMaxSweeps(params, 100));
-    HIPSOLVER_CHECK(hipsolverXsyevjSetTolerance(params, 1.e-7));
-    HIPSOLVER_CHECK(hipsolverXsyevjSetSortEig(params, 1));
+    syevjInfo_t  params;
+    CUSOLVER_CHECK(cusolverDnCreateSyevjInfo(&params));
+    CUSOLVER_CHECK(cusolverDnXsyevjSetMaxSweeps(params, 100));
+    CUSOLVER_CHECK(cusolverDnXsyevjSetTolerance(params, 1.e-7));
+    CUSOLVER_CHECK(cusolverDnXsyevjSetSortEig(params, 1));
 
     // 5. Reserve and copy data to device
     double* d_A    = nullptr;
     double* d_W    = nullptr;
     int*    d_info = nullptr;
 
-    HIP_CHECK(hipMalloc(&d_A, sizeof(double) * A.size()));
-    HIP_CHECK(hipMalloc(&d_W, sizeof(double) * W.size()));
-    HIP_CHECK(hipMalloc(&d_info, sizeof(int)));
-    HIP_CHECK(hipMemcpy(d_A, A.data(), sizeof(double) * A.size(), hipMemcpyHostToDevice));
+    CUDA_CHECK(cudaMalloc(&d_A, sizeof(double) * A.size()));
+    CUDA_CHECK(cudaMalloc(&d_W, sizeof(double) * W.size()));
+    CUDA_CHECK(cudaMalloc(&d_info, sizeof(int)));
+    CUDA_CHECK(cudaMemcpy(d_A, A.data(), sizeof(double) * A.size(), cudaMemcpyHostToDevice));
 
     // 6. Initialize hipsolver
-    hipsolverHandle_t hipsolver_handle;
-    HIPSOLVER_CHECK(hipsolverCreate(&hipsolver_handle));
+    cusolverDnHandle_t cusolver_handle;
+    CUSOLVER_CHECK(cusolverDnCreate(&cusolver_handle));
 
     // 7. Get and reserve the working space on device.
     int     lwork  = 0;
     double* d_work = nullptr;
-    HIPSOLVER_CHECK(
-        hipsolverDsyevj_bufferSize(hipsolver_handle, jobz, uplo, n, d_A, lda, d_W, &lwork, params));
+    CUSOLVER_CHECK(
+        cusolverDnDsyevj_bufferSize(cusolver_handle, jobz, uplo, n, d_A, lda, d_W, &lwork, params));
 
 
     std::cout<< "LL:: 1 lwork = "<<lwork<<"bytes"<<std::endl;
@@ -190,11 +199,11 @@ int main(const int argc, char* argv[])
 
     std::cout<< "LL:: 2 lwork = "<<lwork<<"bytes"<<std::endl;
 
-    HIP_CHECK(hipMalloc(&d_work, sizeof(double) * lwork));
+    CUDA_CHECK(cudaMalloc(&d_work, sizeof(double) * lwork));
 
     // 8. Compute eigenvectors and eigenvalues
 //    HIPSOLVER_CHECK(hipsolverDsyevj(hipsolver_handle,
-    HIPSOLVER_CHECK(hipsolverDnDsyevj(hipsolver_handle,
+    CUSOLVER_CHECK(cusolverDnDsyevj(cusolver_handle,
                                     jobz,
                                     uplo,
                                     n,
@@ -207,9 +216,9 @@ int main(const int argc, char* argv[])
                                     params));
     // 9. Get results from host.
     int info = 0;
-    HIP_CHECK(hipMemcpy(V.data(), d_A, sizeof(double) * V.size(), hipMemcpyDeviceToHost));
-    HIP_CHECK(hipMemcpy(W.data(), d_W, sizeof(double) * W.size(), hipMemcpyDeviceToHost));
-    HIP_CHECK(hipMemcpy(&info, d_info, sizeof(int), hipMemcpyDeviceToHost));
+    CUDA_CHECK(cudaMemcpy(V.data(), d_A, sizeof(double) * V.size(), cudaMemcpyDeviceToHost));
+    CUDA_CHECK(cudaMemcpy(W.data(), d_W, sizeof(double) * W.size(), cudaMemcpyDeviceToHost));
+    CUDA_CHECK(cudaMemcpy(&info, d_info, sizeof(int), cudaMemcpyDeviceToHost));
 
     // 10. Print results
     if(info == 0)
@@ -233,8 +242,8 @@ int main(const int argc, char* argv[])
 
     int    sweeps   = 0;
     double residual = 0;
-    HIPSOLVER_CHECK(hipsolverXsyevjGetSweeps(hipsolver_handle, params, &sweeps));
-    HIPSOLVER_CHECK(hipsolverXsyevjGetResidual(hipsolver_handle, params, &residual));
+    CUSOLVER_CHECK(cusolverDnXsyevjGetSweeps(cusolver_handle, params, &sweeps));
+    CUSOLVER_CHECK(cusolverDnXsyevjGetResidual(cusolver_handle, params, &residual));
 
     std::cout << "\nWhich was computed in " << sweeps << " sweeps, with a residual of " << residual
               << std::endl;
@@ -278,12 +287,12 @@ int main(const int argc, char* argv[])
     std::cout << "\nMean Square Error of [AV == VD]:\n  " << mse << std::endl;
 
     // 12. Clean up device allocations.
-    HIPSOLVER_CHECK(hipsolverDestroy(hipsolver_handle));
-    HIPSOLVER_CHECK(hipsolverDestroySyevjInfo(params));
-    HIP_CHECK(hipFree(d_A));
-    HIP_CHECK(hipFree(d_W));
-    HIP_CHECK(hipFree(d_work));
-    HIP_CHECK(hipFree(d_info));
+    CUSOLVER_CHECK(cusolverDnDestroy(cusolver_handle));
+    CUSOLVER_CHECK(cusolverDnDestroySyevjInfo(params));
+    CUDA_CHECK(cudaFree(d_A));
+    CUDA_CHECK(cudaFree(d_W));
+    CUDA_CHECK(cudaFree(d_work));
+    CUDA_CHECK(cudaFree(d_info));
 
     return report_validation_result(errors);
 }
